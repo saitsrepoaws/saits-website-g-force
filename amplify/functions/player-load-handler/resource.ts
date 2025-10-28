ddf03a96c543c0f3356576497e6e3a6248c63c32
gerard-krom <gerard@guardiangamer.com>
2025-10-28 14:07:31 +0100

    fix: Configure bundling to include AWS SDK modules
    Added bundling config to resource.ts:
    - externalModules: [] (bundle everything)
    - nodeModules: list of AWS SDK packages
    
    This should fix the esbuild resolution error!

--- amplify/functions/player-load-handler/resource.ts
+++ amplify/functions/player-load-handler/resource.ts
@@ -1,4 +1,4 @@
-import { defineFunction, secret } from '@aws-amplify/backend'
+import { defineFunction } from '@aws-amplify/backend'
 
 export const playerLoadHandler = defineFunction({
   name: 'player-load-handler',
@@ -6,4 +6,13 @@ export const playerLoadHandler = defineFunction({
   timeoutSeconds: 30,
   memoryMB: 512,
   runtime: 20,
+  bundling: {
+    externalModules: [], // Bundle ALL modules including AWS SDK
+    nodeModules: [
+      '@aws-sdk/signature-v4',
+      '@aws-sdk/protocol-http',
+      '@aws-sdk/credential-provider-node',
+      '@aws-crypto/sha256-js'
+    ]
+  }
 })
